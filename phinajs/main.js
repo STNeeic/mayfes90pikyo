365af8ce5a742c961b7532e765f13f57f6f4b605
SEKINE AYUMU <stn.dialogue@gmail.com>
2017-02-02 23:12:44 +0900

    phinajsを追加

---
+++ phinajs/main.js
@@ -0,0 +1,27 @@
+// phina.js をグローバル領域に展開
+phina.globalize();
+
+// MainScene クラスを定義
+phina.define('MainScene', {
+  superClass: 'CanvasScene',
+  init: function() {
+    this.superInit();
+    // 背景色を指定
+    this.backgroundColor = '#444';
+    // ラベルを生成
+    this.label = Label('Hello, phina.js!').addChildTo(this);
+    this.label.x = this.gridX.center(); // x 座標
+    this.label.y = this.gridY.center(); // y 座標
+    this.label.fill = 'white'; // 塗りつぶし色
+  },
+});
+
+// メイン処理
+phina.main(function() {
+  // アプリケーション生成
+  var app = GameApp({
+    startLabel: 'main', // メインシーンから開始する
+  });
+  // アプリケーション実行
+  app.run();
+});
