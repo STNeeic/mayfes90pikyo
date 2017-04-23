24e00ce7b6569b1b97fbd4a3b7999413049cea22
SEKINE AYUMU <stn.dialogue@gmail.com>
2017-04-24 07:37:09 +0900

    ゴールが回るようにした #39 test

--- src/js/phina-app/Goal.js
+++ src/js/phina-app/Goal.js
@@ -4,6 +4,9 @@ phina.define('Goal',{
     firstTime: true,
     init: function(){
         this.superInit();
+        this.tweener.by({
+            rotation: 360
+        }, 3000, "swing").setLoop(true);
     },
     reactTo: function(obj, scene){
         if(this.hitTestElement(obj) == true && this.firstTime == true) {
