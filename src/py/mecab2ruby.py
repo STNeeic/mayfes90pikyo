ec867bada7465ebd31aa395baab666c89e887538
SEKINE AYUMU <stn.dialogue@gmail.com>
2017-05-02 20:35:02 +0900

    「振る」の「る」など、送り仮名にルビを振らないように変更した

--- src/py/mecab2ruby.py
+++ src/py/mecab2ruby.py
@@ -12,8 +12,33 @@ while True:
     data = re.split('[\s,]', str)
     if kana.fullmatch(data[0]) == None:
         #この時漢字が含まれているはず
-        out = "<ruby>" + data[0] + "<rp>（</rp><rt>" + ja.kata2hira(data[8]) + "</rt><rp>）</rp></ruby>"
-        print(out, end='')
+        #漢字の部分だけをrubyでくくりたいので漢字を探す
+        hiragana = ja.kata2hira(data[8])
+        offs = 0
+        index = 0
+        while index < len(data[0]):
+            if kana.match(data[0][index]) == None:
+                #基本的に漢字がかなより長くなることはないと仮定する。
+                #又、漢字の読みとそのあとの読みは異なると仮定する。
+                i = 1
+                now = 1
+                while index + i < len(data[0]) and kana.match(data[0][index + i]) == None:
+                    i += 1
+                if index + i < len(data[0]):
+                    while index + offs + now < len(hiragana) and data[0][index + i] != hiragana[offs + now]:
+                        now += 1
+                else:
+                    now = len(hiragana) - index + offs
+                kanji = "".join(data[0][index:index + i])
+                tuzuri = "".join(hiragana[offs:offs + now])
+                out = "<ruby>" + kanji + "<rp>（</rp><rt>" + tuzuri + "</rt><rp>）</rp></ruby>"
+                print(out, end='')
+                offs += now
+                index += i
+            else:
+                print(data[0][index], end='')
+                index += 1
+                offs += 1
     else:
         print(data[0], end='')
 
