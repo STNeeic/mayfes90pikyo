4d547be9f2e5d0cb10a0d7f8605c3e0f7fa0fefd
SEKINE AYUMU <stn.dialogue@gmail.com>
2017-05-02 20:32:35 +0900

    シーンの遷移の仕方を変えた #62

--- src/js/phina-app/main.js
+++ src/js/phina-app/main.js
@@ -1,13 +1,30 @@
  // メイン処理
  phina.main(function() {
      // アプリケーション生成
+     var scenes =[
+     {
+         className: 'TitleScene',
+         label: 'title',
+         nextLabel: 'select'
+     },
+      {
+          className: 'StageSelectScene',
+          label: 'select',
+          nextLabel: 'main'
+      },
+     {
+         className: 'MainScene',
+         label: 'main',
+         nextLabel: 'select'
+     }];
      let app = GameApp({
          startLabel: 'title', // メインシーンから開始する
          assets: ASSETS,
          domElement: document.getElementById("phinaCanvas"),
          width:700,
          height:1050,
-         fit: false 
+         scenes:scenes,
+         fit: false
      });
 
      //appをinitした時点でwidthとheightが決まってしまうので書き換える
@@ -16,7 +33,7 @@
      s.width = "48vh";
      s.height = "auto";
      
-     //app.enableStats();
+     app.enableStats();
      // アプリケーション実行
      app.run();
  });
