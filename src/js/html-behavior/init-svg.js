8fb18aa9a02bfeb331e4e8e7e13f6b117b84b2b9
SEKINE AYUMU <stn.dialogue@gmail.com>
2017-05-05 21:16:54 +0900

    workspaceを保存しておく。又、STARTブロックについてないものはDisable化する fixed #53

--- src/js/html-behavior/init-svg.js
+++ src/js/html-behavior/init-svg.js
@@ -8,9 +8,42 @@
  var toolbox = workspace.toolbox_;
 
 
- //初めからstartblockがworkspaceに置いてあるようにする
- Blockly.Xml.domToWorkspace(document.getElementById('startBlock'),
+//localStorageにworkspaceのセーブデータが置いてあったらそれを用いる
+if(localStorage.workspace != null) {
+    Blockly.Xml.domToWorkspace(Blockly.Xml.textToDom(localStorage.workspace), workspace);
+} else {
+    //初めからstartblockがworkspaceに置いてあるようにする
+    Blockly.Xml.domToWorkspace(document.getElementById('startBlock'),
                             workspace);
+}
+
+let onchange = function(e) {
+    //STARTに繋がってないブロックは透明にしたい
+    let all_blocks = workspace.getAllBlocks();
+    all_blocks.forEach(b => b.setDisabled(true));
+
+    let blk = workspace.getBlockById("START");
+
+    //STARTに繋がっているブロックを網羅するgenerator
+    let blkgen = function* (){
+        let b = blk;
+        while(b != null){
+            yield b;
+            let child = b.getChildren();
+            if(child.length > 0) yield* child;
+            b = b.getNextBlock();
+        }
+    };
+    let gen = blkgen();
+    for (let v of gen) {
+        v.setDisabled(false);
+    }
+
+    //現在のworkspaceをlocalStorageに保存
+    localStorage.workspace = Blockly.Xml.domToText(Blockly.Xml.workspaceToDom(workspace));
+}
+
+workspace.addChangeListener(onchange);
 
  let onresize = function(e) {
      //blocklyAreaの位置を計算する
@@ -43,7 +76,8 @@
      scrollbarDoms.forEach(dom => dom.setAttribute('display', 'none'));
      scrollbarDoms.forEach(dom => dom.setAttribute('display', 'block'));
      Blockly.svgResize(workspace);
- }
+ };
+
  //resize時に呼ばれるように
  window.addEventListener('resize', onresize, false);
  setTimeout(initsvg, 300);
