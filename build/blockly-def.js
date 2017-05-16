4bb2373bcb50a388d0bf3b6b3310880f5192a64e
SEKINE AYUMU <stn.dialogue@gmail.com>
2017-05-16 14:11:01 +0900

    build

--- build/blockly-def.js
+++ build/blockly-def.js
@@ -136,6 +136,31 @@ Blockly.JavaScript['jump'] = function(block){
 };
 
 
+Blockly.Blocks['log_logarea'] = {
+    init: function() {
+        this.jsonInit({
+            "type": "Action",
+            "message0": "%1 を 画面にうつす",
+            "args0": [
+                {
+                    "type": "input_value",
+                    "name": "INPUT"
+                }
+            ],
+            "previousStatement": "Action",
+            "nextStatement": "Action",
+            "tooltip": "指定した数や文字を画面に出力します",
+            "colour": 100
+        });
+    }
+};
+
+Blockly.JavaScript['log_logarea'] = function(block){
+    var input = Blockly.JavaScript.valueToCode(block,"INPUT", Blockly.JavaScript.ORDER_NONE) || "";
+    var code = "$(\"#logArea\").removeClass(\"hide\");\n$(\"#logArea > p\").text(" + input + ");\n";
+    return code;
+};
+
 Blockly.Blocks['is_on_any_marker'] = {
     init: function() {
         this.jsonInit({
@@ -222,13 +247,12 @@ Blockly.Blocks['myvariable_get'] = {
     init: function() {
         this.jsonInit({
             "type": "Action",
-            "message0": "おぼえていた数",
+            "message0": "おぼえていたもの",
             "colour": 330,
-            "output" : "Number",
             "tooltip": "覚えていたもの（数やBoolean）を 出力します",
             "helpUrl": ""
         });
-
+        this.setOutput(true);
     }
 };
 
@@ -246,7 +270,7 @@ Blockly.Blocks['myvariable_set'] = {
             "args0": [
                 {
                     "type": "input_value",
-                    "name": "VAR"
+                    "name": "VALUE"
                 }
             ],
             "colour": 330,
@@ -259,7 +283,7 @@ Blockly.Blocks['myvariable_set'] = {
 };
 
 Blockly.JavaScript['myvariable_set'] = function(block){
-    var input = Blockly.JavaScript.valueToCode(block, "VAR", Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
+    var input = Blockly.JavaScript.valueToCode(block, "VALUE", Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
     console.log(input);
     var code = 'player.variable = ' + input + ";\n";
     return code;
