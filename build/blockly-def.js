460be9cb2e2e4e39a5d57a29545ca42c8742afb0
SEKINE AYUMU <stn.dialogue@gmail.com>
2017-05-21 11:21:09 +0900

    最初のブロックを挟み込む形にする

--- build/blockly-def.js
+++ build/blockly-def.js
@@ -318,6 +318,30 @@ Blockly.JavaScript['run_below'] = function(block){
     return code;
 };
 
+Blockly.Blocks['run_while'] =  {
+    init: function() {
+        this.jsonInit({
+            "type": "block_type",
+            "message0": "ずっとつづける %1",
+            "args0": [
+                {
+                    "type": "input_statement",
+                    "name": "RUNS"
+                }
+            ],
+            "colour": 20,
+            "tooltip": "下にあるブロックをずっと続けます",
+            "helpUrl": ""
+        });
+    }
+};
+
+Blockly.JavaScript['run_while'] = function(block){
+    var input = Blockly.JavaScript.statementToCode(block, "RUNS") || "";
+    var code = input;
+    return code;
+};
+
 Blockly.Blocks['myvariable_get'] = {
     init: function() {
         this.jsonInit({
